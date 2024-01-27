8832bac6cbf69e0d94559c13c1c091e4ed3e0ac6
slartibartfast1234567890 <48054420+slartibartfast1234567890@users.noreply.github.com>
2024-01-27 13:18:26 +0400

    Comments
    Added comments to the Controls.cs script

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -1,25 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Controls: MonoBehaviour
 {
-    private InputActions inputActions;
-    private bool isMoving;
+    private InputActions inputActions; //The object that defines what inputs do what
+    private bool isMoving; //Shows whether the player is holding the left mouse button
 
     private void Awake()
     {
-        inputActions = new InputActions();
+        inputActions = new InputActions(); //Create an instance of the InputActions class
     }
 
     private void OnEnable()
     {
-     
-        inputActions.Enable();
-        inputActions.Player.Start.performed += OnPlayerStartMoving;
-        inputActions.Player.Start.canceled += OnPlayerStopMoving;
-        inputActions.Player.Drag.performed += OnPlayerDragging;
+        inputActions.Enable(); //Enable the InputActions class
+        inputActions.Player.Start.performed += OnPlayerStartMoving; //Add a function to call when the action happens
+        inputActions.Player.Start.canceled += OnPlayerStopMoving;   //Add a function to call when the action happens
+        inputActions.Player.Drag.performed += OnPlayerDragging;     //Add a function to call when the action happens
     }
 
     private void OnDisable()
@@ -29,19 +29,22 @@ public class Controls: MonoBehaviour
         inputActions.Player.Start.canceled -= OnPlayerStopMoving;
     }
 
-    private void OnPlayerStartMoving(InputAction.CallbackContext context) 
+    private void OnPlayerStartMoving(InputAction.CallbackContext context) //Called when we start holding the left mouse button
     { 
+        /* =====TODO=====
+         * 
+         */
         isMoving = context.ReadValueAsButton();
         Debug.Log("button pressed");
     }
 
-    private void OnPlayerStopMoving(InputAction.CallbackContext context)
+    private void OnPlayerStopMoving(InputAction.CallbackContext context) //Called when the player releases the left mouse button
     {
         isMoving = context.ReadValueAsButton();
         Debug.Log("button released");
     }
 
-    private void OnPlayerDragging(InputAction.CallbackContext context)
+    private void OnPlayerDragging(InputAction.CallbackContext context) //Called while the player is moving the mouse
     {
         if(isMoving) 
         {
