56b551c17c9c7bbd6cdfa348f7265afc7e72ed3a
UltimateDecaf <ultimatedecaf@gmail.com>
2024-01-28 14:27:47 +0400

    AudioManager.cs COMPLETE

--- Assets/Scripts/Utils/AudioManager.cs
+++ Assets/Scripts/Utils/AudioManager.cs
@@ -11,10 +11,15 @@ public class AudioManager : MonoBehaviour
 {
 
     //IMPORTANT: create the GameObject for the AudioManager in the Main Menu scene!!! 
-    // In order to add audioClip, you need to go to AudioManager GameObject and add an Audio Source component
+    // In order to add audioClip:
+    // 1. Create a SerializeField
+    // 2. Insert your AudioClip from the Assets folder
+    // 3. Write a method for playing it.
+    // 4. Reference it in the appropriate section of the state script
     public static AudioManager Instance { get; private set; }
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip testMusic;
+    [SerializeField] private AudioClip testSuccessMusic;
 
     private void Start()
     {
@@ -25,8 +30,11 @@ public class AudioManager : MonoBehaviour
         else
         {
             Instance = this;
-        }
+        }  
+    }
 
+    public void PlayTestSound() 
+    {
         audioSource.PlayOneShot(testMusic);
     }
    
